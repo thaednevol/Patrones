6779219714f7c998d11a412a98654434733499b3
Edwin Hurtado <ehurtado@ehurtado>
2016-12-11 15:18:51 -0500

    Composite

---
+++ Composite/src/FileComponent.java
@@ -0,0 +1,14 @@
+public class FileComponent extends FileSystemComponent {
+  private long size;
+
+  public FileComponent(String cName, long sz) {
+    super(cName);
+    size = sz;
+  }
+
+  public long getComponentSize() {
+    return size;
+  }
+
+} // End of class
+
