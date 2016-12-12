76459a75c990cf837392b9915920220cc906639d
Edwin Hurtado <ehurtado@ehurtado>
2016-12-12 14:17:24 -0500

    Adapter

---
+++ Adapter/src/cls/CAAddressAdapter.java
@@ -0,0 +1,12 @@
+package cls;
+
+public class CAAddressAdapter extends CAAddress 
+	implements AddressValidator {
+
+  public boolean isValidAddress(String inp_address,String inp_zip, String inp_state) {
+
+    return isValidCanadianAddr(inp_address, inp_zip,inp_state);
+  }
+}// end of class
+
+
---
+++ Adapter/src/obj/AddressValidator.java
@@ -0,0 +1,8 @@
+package obj;
+
+public abstract class AddressValidator {
+  public abstract boolean isValidAddress(String inp_address,
+      String inp_zip, String inp_state);
+}// end of class
+
+
