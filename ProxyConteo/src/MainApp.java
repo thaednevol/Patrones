2b32e4d66f1ff57ff42b9f9e0221fc2790333d25
Edwin Hurtado <ehurtado@ehurtado>
2016-12-12 23:16:15 -0500

    Proxy conteo

---
+++ ProxyConteo/src/MainApp.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class MainApp {
+@SuppressWarnings("unused")
+public static void main(String[] args) {
+    OrderIF order = new OrderProxy();
+    Vector<?> v = order.getAllOrders();
+    v = order.getAllOrders();
+    v = order.getAllOrders();
+    v = order.getAllOrders();
+  }
+}
