3e17e22a32e2840b50d8a0d855658007355ab982
Edwin Hurtado <ehurtado@ehurtado>
2016-12-12 22:58:26 -0500

    Singleton

---
+++ Singleton/src/ConsoleLogger.java
@@ -0,0 +1,9 @@
+package src;
+
+public class ConsoleLogger implements Logger {
+
+  public void log(String msg) {
+    System.out.println(msg);
+  }
+
+}
---
+++ Singleton/src/Logger.java
@@ -0,0 +1,7 @@
+package src;
+
+public interface Logger {
+
+  public void log(String msg);
+
+}
